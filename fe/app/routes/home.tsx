208b6e1551ab6441049eabecdfb57f2a63fd7f27
AkAdmin68 <talkwithak@outlook.in>
2025-08-02 10:26:02 +0530

    hello world react

---
+++ fe/app/routes/home.tsx
@@ -0,0 +1,15 @@
+import type { Route } from "./+types/home";
+import { Welcome } from "../welcome/welcome";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return <div className="flex-1 flex flex-col items-center justify-center">
+    Hello world
+  </div>;
+}
