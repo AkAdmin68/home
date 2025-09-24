33657b14a8ac95a4b10633429b142fd7f0f0694e
AkAdmin68 <talkwithak@outlook.in>
2025-09-24 19:03:02 +0530

    basic routing

--- fe-static/src/App.tsx
+++ fe-static/src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+const Home = () => <h2>Home Page</h2>;
+const About = () => <h2>About Page</h2>;
+const Contact = () => <h2>Contact Page</h2>;
 
 function App() {
   return (
-    <div className="App">
-      Hello world from AK.
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route index path="/" element={<Home></Home>} />
+        <Route path="/about" element={<About></About>} />
+        <Route path="/contact" element={<Contact></Contact>} />
+        <Route path="*" element={<h2>404 - Page Not Found</h2>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
