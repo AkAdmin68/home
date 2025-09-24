7e65fcc669b357fc0a8256944f35298fef23fc28
AkAdmin68 <talkwithak@outlook.in>
2025-09-24 19:37:41 +0530

    basic routing with vite

---
+++ react-vite/src/router.tsx
@@ -0,0 +1,21 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import App from './App';
+import About from './Components/About/About';
+import Contact from './Components/Contact/COntact';
+
+function Router() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/contact">Contact</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default Router;
