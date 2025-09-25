c431359e00875c271f20598d488120c8cadcbe29
AkAdmin68 <talkwithak@outlook.in>
2025-09-25 13:41:58 +0530

    change

--- fe/src/Components/Contact/Contact.tsx
+++ fe/src/Components/Contact/Contact.tsx
@@ -1,3 +1,3 @@
-const Contact = () => <h1>Contact content</h1>;
+const Contact = () => <h1 className="text-3xl font-bold underline">Contact content</h1>;
 
 export default Contact;
--- fe/src/Components/Courses/Courses.tsx
+++ fe/src/Components/Courses/Courses.tsx
@@ -1,3 +1,3 @@
-const Courses = () => <h1>Courses content</h1>;
+const Courses = () => <h1 className="text-3xl font-bold underline">Courses content</h1>;
 
 export default Courses;
