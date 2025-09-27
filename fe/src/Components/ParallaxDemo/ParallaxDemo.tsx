d325b8d0ee48d03e454083d4e6c6c4cabc081972
AkAdmin68 <talkwithak@outlook.in>
2025-09-27 17:46:03 +0530

    parallax with nav

--- fe/src/Components/ParallaxDemo/ParallaxDemo.tsx
+++ fe/src/Components/ParallaxDemo/ParallaxDemo.tsx
@@ -14,24 +14,9 @@ export default function ParallaxDemo() {
   const [customElement, setCustomElement] = useState<HTMLDivElement>();
   const { scrollDir } = useDetectScroll({ axis: Axis.Y, target: customElement });
   const [pageNo, setPageNo] = useState(0);
-  const PAGES = 3;
-
-  // useEffect(() => {
-  //   // You can call scrollTo here after the component mounts
-  //   // For example, to scroll to page 1 (index 0) after a delay
-  //   const timer = setTimeout(() => {
-  //     if (parallax.current) {
-  //       parallax.current.scrollTo(1); // Scrolls to the second page (index 1)
-  //     }
-  //   }, 0);
-
-  //   // Cleanup the timeout if the component unmounts before the scroll
-  //   return () => clearTimeout(timer);
-  // }, []); 
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      console.log(parallax.current, parallax.current?.container?.current);
       if(parallax.current) {
           setCustomElement(parallax.current.container.current);
       }
@@ -53,8 +38,8 @@ export default function ParallaxDemo() {
     const timer = setTimeout(() => {
       switch (scrollDir) {
         case Direction.Down: {
-          // console.log("down", Math.ceil(parallax.current.current / (2 * parallax.current.space) * (PAGES - 1)))
-          const nextPage = Math.ceil(parallax.current.current / (2 * parallax.current.space) * (PAGES - 1))
+          // console.log("down", Math.ceil(parallax.current.current / parallax.current.space))
+          const nextPage = Math.ceil(parallax.current.current / parallax.current.space)
           if (nextPage !== pageNo) {
             setPageNo(nextPage);
             parallax.current.scrollTo(nextPage);
@@ -62,8 +47,8 @@ export default function ParallaxDemo() {
           break;
         }
         case Direction.Up: {
-          // console.log("up", Math.floor(parallax.current.current / (2 * parallax.current.space) * (PAGES - 1)))
-          const previousPage = Math.floor(parallax.current.current / (2 * parallax.current.space) * (PAGES - 1))
+          // console.log("up", Math.ceil(parallax.current.current / parallax.current.space))
+          const previousPage = Math.floor(parallax.current.current / parallax.current.space)
           if (previousPage !== pageNo) {
             setPageNo(previousPage);
             parallax.current.scrollTo(previousPage);
@@ -71,29 +56,31 @@ export default function ParallaxDemo() {
           break;
         }
       }
-      // console.log('scrollDir', scrollDir, Math.round(parallax.current.current / (2 * parallax.current.space) * 2));
     }, 50);
     return () => clearTimeout(timer);
-    // if (scrollDir === Direction.Down) {
-    //   const nextPage = Math.min(parallax.current.offset + 1, 2);
-    //   console.log('nextPage', nextPage);
-    //   parallax.current.scrollTo(nextPage);
-    // } else if (scrollDir === Direction.Up) {
-    //   const previousPage = Math.max(parallax.current.offset - 1, 0);
-    //   console.log('previousPage', previousPage);
-    //   parallax.current.scrollTo(previousPage);
-    // } 
   }, [parallax.current?.current]);
 
   return (
-    <div style={{ width: '100%', height: '100%', background: '#253237' }}>
-      <Parallax ref={parallax} pages={3} style={{ scrollSnapType: 'y mandatory' }}>
+    <Parallax ref={parallax} pages={7} style={{ position: "relative", height: '100%' }}>
 
-        <ParallaxLayer offset={0}  style={{ backgroundColor: '#509b5aff', scrollSnapAlign: 'center' }} />
-        <ParallaxLayer offset={1}  style={{ backgroundColor: '#805E73', scrollSnapAlign: 'center' }} />
-        <ParallaxLayer offset={2}  style={{ backgroundColor: '#87BCDE', scrollSnapAlign: 'center' }} />
+        <ParallaxLayer offset={0}  style={{ backgroundColor: '#509b5aff'}} />
+        <ParallaxLayer offset={1}  style={{ backgroundColor: '#805E73'}} />
+        <ParallaxLayer offset={2}  style={{ backgroundColor: '#87BCDE'}} />
+        
+        <ParallaxLayer offset={3}  style={{ backgroundColor: '#509b5aff'}} />
+        <ParallaxLayer offset={4}  style={{ backgroundColor: '#805E73'}} />
+        <ParallaxLayer offset={5}  style={{ backgroundColor: '#87BCDE'}} />
+        
+        <ParallaxLayer offset={6}  style={{ backgroundColor: '#509b5aff'}} />
 
-        {/* <ParallaxLayer
+      </Parallax>
+  )
+}
+
+/* <div style={{ width: '100%', height: '100%', background: '#253237' }}>
+      
+
+      {/* <ParallaxLayer
           offset={0}
           speed={0}
           factor={3}
@@ -101,7 +88,7 @@ export default function ParallaxDemo() {
             backgroundImage: url('stars', true),
             backgroundSize: 'cover',
           }}
-        /> */}
+        />
 
         {/* <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
           <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} />
@@ -190,8 +177,5 @@ export default function ParallaxDemo() {
           onClick={() => { console.log(parallax.current); parallax.current.scrollTo(0) }}
           >
           <img src={url('clients-main')} style={{ width: '40%' }} />
-        </ParallaxLayer> */}
-      </Parallax>
-    </div>
-  )
-}
+        </ParallaxLayer>
+    </div> */
--- fe/src/router.tsx
+++ fe/src/router.tsx
@@ -7,18 +7,20 @@ import ParallaxDemo from './Components/ParallaxDemo/ParallaxDemo';
 function Router() {
   return (
     <div>      
-      <nav>
+      <nav style={{height: "5vh", display: "flex", alignItems: "center", justifyContent: "center", gap: "10px", backgroundColor: "#f0f0f0", position: "sticky", top: 0, zIndex: 1000}}>
         <Link to="/">Courses</Link> 
         | <Link to="/about">About</Link> 
         | <Link to="/contact">Contact</Link>
         | <Link to="/parallax">ParallaxDemo</Link>
       </nav>
-      <Routes>
-        <Route path="/" element={<Courses />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/parallax" element={<ParallaxDemo />} />
-      </Routes>
+      <div style={{height: "95vh"}}>
+        <Routes>
+          <Route path="/" element={<Courses />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/parallax" element={<ParallaxDemo />} />
+        </Routes>
+      </div>
     </div>
   );
 }
