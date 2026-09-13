aef9cb9b3123fd8cd0168187247d1ebe4731943f
AkAdmin68 <talkwithak@outlook.in>
2026-09-13 12:43:28 +0530

    Updates

---
+++ assets/Contact-CeBaOiaA.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BlanqfUn.js";const n=()=>t.jsx("h1",{className:"tw-qc tw-cc underline",children:"Contact content"});export{n as default};
---
+++ assets/Courses-vZGYbCQT.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-BlanqfUn.js";const t=()=>s.jsx("h1",{className:"tw-qc tw-cc underline",children:"Courses content"});export{t as default};
