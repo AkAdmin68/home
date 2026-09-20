945413c6a3350b8a013017311d328f265732b3ca
AkAdmin68 <talkwithak@outlook.in>
2026-09-20 15:28:13 +0530

    Updates

---
+++ assets/Contact-27MwmGgp.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DhA1FzJ-.js";const n=()=>t.jsx("h1",{className:"tw-qc tw-cc underline",children:"Contact content"});export{n as default};
--- assets/Courses-gxTfEtSp.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-DBwKOnGf.js";const t=()=>s.jsx("h1",{className:"tw-qc tw-cc underline",children:"Courses content"});export{t as default};
